5c2422cf8ba14507bc26e7014d9d59f8532cb52e
Juan Carlos García <jugade92@gmail.com>
2015-09-09 19:08:04 +0200

    Updating Androoid Field commands to generate different types of fields

--- addon-fields/src/main/java/io/androoid/roo/addon/suite/addon/fields/AndrooidFieldsCommands.java
+++ addon-fields/src/main/java/io/androoid/roo/addon/suite/addon/fields/AndrooidFieldsCommands.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
-import org.springframework.roo.classpath.TypeLocationService;
 import org.springframework.roo.model.JavaSymbolName;
 import org.springframework.roo.model.JavaType;
 import org.springframework.roo.shell.CliAvailabilityIndicator;
@@ -16,8 +15,8 @@ import org.springframework.roo.shell.CliOption;
 import org.springframework.roo.shell.CommandMarker;
 
 /**
- * Androoid Fields commands class. This class provides all necessary commands
- * to generate entity fields on Android Project.
+ * Androoid Fields commands class. This class provides all necessary commands to
+ * generate entity fields on Android Project.
  * 
  * 
  * @author Juan Carlos García
@@ -34,30 +33,59 @@ public class AndrooidFieldsCommands implements CommandMarker {
 
 	@Reference
 	private AndrooidFieldsOperations fieldOperations;
-	
+
 	/**
 	 * Field generation is only available if persistence was configured using
 	 * androoid persistence command.
 	 * 
 	 * @return true if persistence is installed.
 	 */
-	@CliAvailabilityIndicator("androoid field")
+	@CliAvailabilityIndicator({ "androoid field string", "androoid field number", "androoid field boolean",
+			"androoid field geo" })
 	public boolean isEntityCreationAvailable() {
 		return fieldOperations.isFieldCreationAvailable();
 	}
 
 	/**
-	 * This method registers the androoid field command.
+	 * This method registers the androoid field string command.
 	 * 
 	 * @param class
 	 */
-	@CliCommand(value = "androoid field", help = "Creates new field on selected entity.")
-	public void createEntity(
-			@CliOption(key = "entity", optionContext = UPDATE_PROJECT, mandatory = true, help = "Name of the entity where generated field will be added. (Ex: ~.domain.MyEntity)") final JavaType entity,
-			@CliOption(key = "name", mandatory = false, optionContext = UPDATE_PROJECT, help = "The field name to use.") final JavaSymbolName fieldName,
-			@CliOption(key = "type", mandatory = true, optionContext = "java-lang,project", help = "The field type that will be used for the field.") final JavaType fieldType) {
-		
+	@CliCommand(value = "androoid field string", help = "Creates new String field on selected entity.")
+	public void createStringField(
+			@CliOption(key = "class", mandatory = false, unspecifiedDefaultValue = "*", optionContext = UPDATE_PROJECT, help = "The name of the entity to receive this field") final JavaType entity,
+			@CliOption(key = "name", mandatory = true, optionContext = UPDATE_PROJECT, help = "The field name to use.") final JavaSymbolName fieldName) {
+
+		// Creating new field
+		fieldOperations.createField(entity, fieldName, JavaType.STRING);
+	}
+
+	/**
+	 * This method registers the androoid field number command.
+	 * 
+	 * @param class
+	 */
+	@CliCommand(value = "androoid field number", help = "Creates new numeric field on selected entity.")
+	public void createNumberField(
+			@CliOption(key = "class", mandatory = false, unspecifiedDefaultValue = "*", optionContext = UPDATE_PROJECT, help = "The name of the entity to receive this field") final JavaType entity,
+			@CliOption(key = "type", mandatory = true, optionContext = "java-number", help = "The Java type of the numeric field") JavaType fieldType,
+			@CliOption(key = "name", mandatory = true, optionContext = UPDATE_PROJECT, help = "The field name to use.") final JavaSymbolName fieldName) {
+
 		// Creating new field
 		fieldOperations.createField(entity, fieldName, fieldType);
+	}
+
+	/**
+	 * This method registers the androoid field boolean command.
+	 * 
+	 * @param class
+	 */
+	@CliCommand(value = "androoid field boolean", help = "Creates new boolean on selected entity.")
+	public void createBooleanField(
+			@CliOption(key = "class", mandatory = false, unspecifiedDefaultValue = "*", optionContext = UPDATE_PROJECT, help = "The name of the entity to receive this field") final JavaType entity,
+			@CliOption(key = "name", mandatory = true, optionContext = UPDATE_PROJECT, help = "The field name to use.") final JavaSymbolName fieldName) {
+
+		// Creating new field
+		fieldOperations.createField(entity, fieldName, JavaType.BOOLEAN_PRIMITIVE);
 	}
 }
