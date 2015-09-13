607771e9f4d2b6f14e506d268f7dafcba46bd1ca
Juan Carlos García <jugade92@gmail.com>
2015-09-13 12:59:07 +0200

    Generating main activity methods

---
+++ addon-activities/src/main/java/io/androoid/roo/addon/suite/addon/activities/AndrooidMainActivityMetadata.java
@@ -0,0 +1,143 @@
+package io.androoid.roo.addon.suite.addon.activities;
+
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.roo.classpath.PhysicalTypeIdentifierNamingUtils;
+import org.springframework.roo.classpath.PhysicalTypeMetadata;
+import org.springframework.roo.classpath.details.MethodMetadataBuilder;
+import org.springframework.roo.classpath.details.annotations.AnnotationMetadataBuilder;
+import org.springframework.roo.classpath.itd.AbstractItdTypeDetailsProvidingMetadataItem;
+import org.springframework.roo.classpath.itd.InvocableMemberBodyBuilder;
+import org.springframework.roo.metadata.MetadataIdentificationUtils;
+import org.springframework.roo.model.JavaPackage;
+import org.springframework.roo.model.JavaSymbolName;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.project.LogicalPath;
+
+import io.androoid.roo.addon.suite.addon.activities.annotations.AndrooidMainActivity;
+
+/**
+ * Metadata for {@link AndrooidMainActivity}.
+ * 
+ * @author Juan Carlos García
+ * @since 1.0
+ */
+public class AndrooidMainActivityMetadata extends AbstractItdTypeDetailsProvidingMetadataItem {
+
+	private static final String PROVIDES_TYPE_STRING = AndrooidMainActivityMetadata.class.getName();
+	private static final String PROVIDES_TYPE = MetadataIdentificationUtils.create(PROVIDES_TYPE_STRING);
+
+	private JavaPackage applicationPackage;
+
+	public static String createIdentifier(final JavaType javaType, final LogicalPath path) {
+		return PhysicalTypeIdentifierNamingUtils.createIdentifier(PROVIDES_TYPE_STRING, javaType, path);
+	}
+
+	public static JavaType getJavaType(final String metadataIdentificationString) {
+		return PhysicalTypeIdentifierNamingUtils.getJavaType(PROVIDES_TYPE_STRING, metadataIdentificationString);
+	}
+
+	public static String getMetadataIdentiferType() {
+		return PROVIDES_TYPE;
+	}
+
+	public static LogicalPath getPath(final String metadataIdentificationString) {
+		return PhysicalTypeIdentifierNamingUtils.getPath(PROVIDES_TYPE_STRING, metadataIdentificationString);
+	}
+
+	public static boolean isValid(final String metadataIdentificationString) {
+		return PhysicalTypeIdentifierNamingUtils.isValid(PROVIDES_TYPE_STRING, metadataIdentificationString);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param identifier
+	 *            the ID of the metadata to create (must be a valid ID)
+	 * @param aspectName
+	 *            the name of the ITD to be created (required)
+	 * @param governorPhysicalTypeMetadata
+	 *            the governor (required)
+	 */
+	public AndrooidMainActivityMetadata(final String identifier, final JavaType aspectName,
+			final PhysicalTypeMetadata governorPhysicalTypeMetadata, JavaPackage applicationPackage) {
+		super(identifier, aspectName, governorPhysicalTypeMetadata);
+		Validate.isTrue(isValid(identifier), "Metadata identification string '%s' does not appear to be a valid",
+				identifier);
+
+		this.applicationPackage = applicationPackage;
+
+		// Generating main method
+		builder.addMethod(getOnCreateMethod());
+
+		// Create a representation of the desired output ITD
+		itdTypeDetails = builder.build();
+
+	}
+
+	/**
+	 * Generates onCreate method
+	 * 
+	 * @return onCreate Method Metadata Builder
+	 */
+	private MethodMetadataBuilder getOnCreateMethod() {
+
+		// Compute the mutator method name
+		final JavaSymbolName methodName = new JavaSymbolName("onCreate");
+
+		// See if the type itself declared the main method
+		if (governorHasMethod(methodName)) {
+			return null;
+		}
+
+		final InvocableMemberBodyBuilder bodyBuilder = new InvocableMemberBodyBuilder();
+		buildOnCreateMethodBody(bodyBuilder);
+
+		MethodMetadataBuilder methodBuilder = new MethodMetadataBuilder(getId(), Modifier.PUBLIC, methodName,
+				JavaType.VOID_PRIMITIVE, bodyBuilder);
+
+		// Adding params
+		methodBuilder.addParameter("savedInstanceState", new JavaType("android.os.Bundle"));
+
+		// Adding annotations
+		List<AnnotationMetadataBuilder> annotations = new ArrayList<AnnotationMetadataBuilder>();
+		annotations.add(new AnnotationMetadataBuilder(new JavaType("Override")));
+		methodBuilder.setAnnotations(annotations);
+
+		return methodBuilder;
+
+	}
+
+	/**
+	 * Generates onCreate method body
+	 * 
+	 * @param bodyBuilder
+	 */
+	private void buildOnCreateMethodBody(InvocableMemberBodyBuilder bodyBuilder) {
+		// super.onCreate(savedInstanceState);
+		bodyBuilder.appendFormalLine("super.onCreate(savedInstanceState);");
+
+		// setContentView(R.layout.main_activity);
+		bodyBuilder.appendFormalLine(String.format("setContentView(%s.layout.main_activity);",
+				new JavaType(applicationPackage.getFullyQualifiedPackageName().concat(".R"))
+						.getNameIncludingTypeParameters(false, builder.getImportRegistrationResolver())));
+
+		// TODO: Include buttons for annotation registered entities
+	}
+
+	@Override
+	public String toString() {
+		final ToStringBuilder builder = new ToStringBuilder(this);
+		builder.append("identifier", getId());
+		builder.append("valid", valid);
+		builder.append("aspectName", aspectName);
+		builder.append("destinationType", destination);
+		builder.append("governor", governorPhysicalTypeMetadata.getId());
+		builder.append("itdTypeDetails", itdTypeDetails);
+		return builder.toString();
+	}
+}
---
+++ addon-activities/src/main/java/io/androoid/roo/addon/suite/addon/activities/AndrooidMainActivityMetadataProvider.java
@@ -0,0 +1,84 @@
+package io.androoid.roo.addon.suite.addon.activities;
+
+import java.util.logging.Logger;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.service.component.ComponentContext;
+import org.springframework.roo.classpath.PhysicalTypeIdentifier;
+import org.springframework.roo.classpath.PhysicalTypeMetadata;
+import org.springframework.roo.classpath.itd.AbstractItdMetadataProvider;
+import org.springframework.roo.classpath.itd.ItdTypeDetailsProvidingMetadataItem;
+import org.springframework.roo.model.JavaPackage;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.project.LogicalPath;
+import org.springframework.roo.project.ProjectOperations;
+import org.springframework.roo.support.logging.HandlerUtils;
+
+import io.androoid.roo.addon.suite.addon.activities.annotations.AndrooidMainActivity;
+
+/**
+ * Provides {@link AndrooidMainActivityMetadata}.
+ * 
+ * @author Juan Carlos García
+ * @since 1.0
+ */
+@Component
+@Service
+public class AndrooidMainActivityMetadataProvider extends AbstractItdMetadataProvider {
+
+	protected final static Logger LOGGER = HandlerUtils.getLogger(AndrooidMainActivityMetadataProvider.class);
+
+	public static final JavaType ANDROOID_MAIN_ACTVITY = new JavaType(AndrooidMainActivity.class);
+
+	@Reference
+	private ProjectOperations projectOperations;
+
+	protected void activate(final ComponentContext cContext) {
+		context = cContext.getBundleContext();
+		getMetadataDependencyRegistry().addNotificationListener(this);
+		getMetadataDependencyRegistry().registerDependency(PhysicalTypeIdentifier.getMetadataIdentiferType(),
+				getProvidesType());
+		addMetadataTrigger(ANDROOID_MAIN_ACTVITY);
+	}
+
+	@Override
+	protected String createLocalIdentifier(final JavaType javaType, final LogicalPath path) {
+		return AndrooidMainActivityMetadata.createIdentifier(javaType, path);
+	}
+
+	protected void deactivate(final ComponentContext context) {
+		getMetadataDependencyRegistry().removeNotificationListener(this);
+		getMetadataDependencyRegistry().deregisterDependency(PhysicalTypeIdentifier.getMetadataIdentiferType(),
+				getProvidesType());
+		removeMetadataTrigger(ANDROOID_MAIN_ACTVITY);
+	}
+
+	@Override
+	protected String getGovernorPhysicalTypeIdentifier(final String metadataIdentificationString) {
+		final JavaType javaType = AndrooidMainActivityMetadata.getJavaType(metadataIdentificationString);
+		final LogicalPath path = AndrooidMainActivityMetadata.getPath(metadataIdentificationString);
+		return PhysicalTypeIdentifier.createIdentifier(javaType, path);
+	}
+
+	public String getItdUniquenessFilenameSuffix() {
+		return "AndrooidMainActivity";
+	}
+
+	@Override
+	protected ItdTypeDetailsProvidingMetadataItem getMetadata(final String metadataIdentificationString,
+			final JavaType aspectName, final PhysicalTypeMetadata governorPhysicalTypeMetadata,
+			final String itdFilename) {
+
+		JavaPackage applicationPackage = projectOperations.getFocusedTopLevelPackage();
+
+		return new AndrooidMainActivityMetadata(metadataIdentificationString, aspectName, governorPhysicalTypeMetadata,
+				applicationPackage);
+	}
+
+	public String getProvidesType() {
+		return AndrooidMainActivityMetadata.getMetadataIdentiferType();
+	}
+
+}
